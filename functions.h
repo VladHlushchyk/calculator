86db825232d2cfaf7ba1696808e460bdaba1c860
VladHlushchyk <vladglusik4@gmail.com>
2026-08-21 18:01:04 +0300

    Initial commit

---
+++ functions.h
@@ -0,0 +1,47 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include "header.h"
+
+
+// Mathematical operations(i call them actions)
+double add          (double, double);
+double substract    (double, double);
+double multiply     (double, double);
+double divide       (double, double);
+
+/*
+    Finds action by symbol in 
+        extern const Action actions[];
+    and returns ID of it or -1(if failure)
+*/
+int find_action(char symbol);
+
+
+// Parser, and functions to parse
+/*
+    Reads first non-space letter;
+    Returns readed char, EXIT_SUCCESS(if reach end of std_in) or EXIT_FAILURE
+*/
+char getC(void);
+
+/*
+    Reads number from the flow (of nodes), and returns it as double
+*/
+double get_num(char *c);
+
+/*
+    Parses all the date from std_in, and creates flow of nodes that contain it.
+    Returns NULL(if failure) or first node of the flow
+*/
+Node *parser(void);
+
+// Solver
+/*
+    Solves the example, that is already parsed.
+    Needs first_node of the node-flow to work correctly
+    Returns the result of example, or EXIT_FAILURE
+*/
+double solver(Node *first_node);
+
+#endif
---
+++ header.h
@@ -0,0 +1,54 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+
+// Header files
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+
+// define values
+#define BUF_SIZE 128
+
+
+// Custom types/structures
+typedef struct Node {
+    struct Node *next;
+    struct Node *prev;      //prev is previous for short
+    double value;
+    char type;
+} Node;
+/*types:
+    'n' - number;
+    'b' - bracket;
+    'a' - action like +, *... It takes two numbers: the one before, and one after it;
+*/
+
+typedef double (*Func_ptr)(double, double); 
+
+typedef struct {
+    char symbol;                        //The symbol to identify the action
+    char type;
+    int priority;
+    Func_ptr action;
+} Action;
+
+
+// Consts
+/*
+    Array, where all actions is stored.
+    You also can add your custom action, it only must have 2 inputs(double a, b) and return double value
+*/
+extern const Action actions[];
+
+extern const int actions_am;        // actions_am is amount of actions
+
+
+#include "functions.h"
+
+#endif
